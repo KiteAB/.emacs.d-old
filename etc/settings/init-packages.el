af60bdb10491b3869641933d9a05f64c51da5540
David Zhang <kiteabpl@outlook.com>
2020-09-19 23:17:30 +0800

    rainbow-delimiters

--- etc/settings/init-packages.el
+++ etc/settings/init-packages.el
@@ -244,7 +244,26 @@
 ;;; Rainbow Delimiters
 (use-package rainbow-delimiters
  :ensure t
- :hook ((lisp-mode-hook emacs-lisp-mode-hook org-mode-hooke eshell-mode-hook) . rainbow-delimiters-mode))
+ :hook ((lisp-mode-hook emacs-lisp-mode-hook org-mode-hooke eshell-mode-hook) . rainbow-delimiters-mode)
+ :config
+  (set-face-foreground 'rainbow-delimiters-depth-1-face "chartreuse3")
+  (set-face-foreground 'rainbow-delimiters-depth-2-face "DodgerBlue1")
+  (set-face-foreground 'rainbow-delimiters-depth-3-face "DarkOrange2")
+  (set-face-foreground 'rainbow-delimiters-depth-4-face "deep pink")
+  (set-face-foreground 'rainbow-delimiters-depth-5-face "medium orchid")
+  (set-face-foreground 'rainbow-delimiters-depth-6-face "turquoise")
+  (set-face-foreground 'rainbow-delimiters-depth-7-face "lime green")
+  (set-face-foreground 'rainbow-delimiters-depth-8-face "gold")
+  (set-face-foreground 'rainbow-delimiters-depth-9-face "cyan")
+  (set-face-bold 'rainbow-delimiters-depth-1-face "t")
+  (set-face-bold 'rainbow-delimiters-depth-2-face "t")
+  (set-face-bold 'rainbow-delimiters-depth-3-face "t") 
+  (set-face-bold 'rainbow-delimiters-depth-4-face "t")
+  (set-face-bold 'rainbow-delimiters-depth-5-face "t")
+  (set-face-bold 'rainbow-delimiters-depth-6-face "t")
+  (set-face-bold 'rainbow-delimiters-depth-7-face "t")
+  (set-face-bold 'rainbow-delimiters-depth-8-face "t")
+  (set-face-bold 'rainbow-delimiters-depth-9-face "t"))
 
 ;;; Indent Guide
 (use-package indent-guide
