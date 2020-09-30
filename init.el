00453d97fc9f0c51add74f6ee55340d003cccd3f
David Zhang <kiteabpl@outlook.com>
2020-09-30 19:29:35 +0800

    ascii

--- init.el
+++ init.el
@@ -1,4 +1,12 @@
-;;;;; KiteAB's Personal Emacs Configuration File
+;;  __  __         _____
+;; |  \/  |_   _  | ____|_ __ ___   __ _  ___ ___
+;; | |\/| | | | | |  _| | '_ ` _ \ / _` |/ __/ __|
+;; | |  | | |_| | | |___| | | | | | (_| | (__\__ \
+;; |_|  |_|\__, | |_____|_| |_| |_|\__,_|\___|___/
+;;         |___/
+
+;; Author: KiteAB (https://github.com/KiteAB)
+;; Repository: https://github.com/KiteAB/.emacs.d
 
 ;;; Benchmark
 ;(load-file "~/.emacs.d/benchmark.el")
