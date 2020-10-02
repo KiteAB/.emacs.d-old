16b25797296c6e6cbb05d9de4cee6de9177eb605
David Zhang <kiteabpl@outlook.com>
2020-10-02 09:12:42 +0800

    removed filetype scan

--- etc/settings/init-packages.el
+++ etc/settings/init-packages.el
@@ -401,21 +401,6 @@
 (use-package dired
 	:ensure nil
 	:config
-	(setq dired-guess-shell-alist-user `((,(rx "."
-																						 (or
-																							;; Videos
-																							"mp4" "avi" "mkv" "flv" "ogv" "mov"
-																							;; Music
-																							"wav" "mp3" "flac"
-																							;; Images
-																							"jpg" "jpeg" "png" "gif" "xpm" "svg" "bmp"
-																							;; Docs
-																							"pdf" "md" "djvu" "ps" "eps")
-																						 string-end)
-																				,(cond ((eq system-type 'gnu/linux) "xdg-open")
-																							 ((eq system-type 'darwin) "open")
-																							 ((eq system-type 'windows-nt) "start")
-																							 (t "")))))
 	(define-advice dired-do-print (:override (&optional _))
     "Show/hide dotfiles."
     (interactive)
