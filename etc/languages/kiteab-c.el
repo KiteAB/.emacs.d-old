6edd3b9540cbe4e1b65e31adb86e0226f87600e6
David Zhang <kiteabpl@outlook.com>
2020-10-03 10:46:07 +0800

    c settings

--- etc/languages/kiteab-c.el
+++ etc/languages/kiteab-c.el
@@ -1,6 +1,8 @@
 ;;;; This file is the configuration about C/C++.
 
-(setq c-default-style "linux"
-			c-basic-offset 2)
+(setq-default c-default-style '((java-mode . "java")
+																(awk-mode . "awk")
+																(other . "linux"))
+							c-basic-offset 2)
 
 (provide 'kiteab-c)
