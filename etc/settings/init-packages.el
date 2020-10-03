546f9d18bf9e27143a7e03a8da5432f03306dee8
David Zhang <kiteabpl@outlook.com>
2020-10-03 21:27:21 +0800

    use-package

--- etc/settings/init-packages.el
+++ etc/settings/init-packages.el
@@ -3,15 +3,18 @@
 (define-prefix-command 'plugin-key)
 (global-set-key (kbd "C-'") 'plugin-key)
 
+;;; Init Packages
+(package-initialize)
+
 ;;; Mirror
 (require 'package)
 (require 'cl-lib) ; Common Lisp
 (setq package-archives '(("gnu" . "http://mirrors.bfsu.edu.cn/elpa/gnu/")
 												 ("melpa" . "http://mirrors.bfsu.edu.cn/elpa/melpa/")))
 ;;; Init Use-package
-(eval-when-compile
-  (add-to-list 'load-path "~/.emacs.d/site-lisp/use-package")
-  (require 'use-package))
+(unless (package-installed-p 'use-package)
+	(package-refresh-contents)
+	(package-install 'use-package))
 (setq use-package-hook-name-suffix nil)
 
 ;;; Third Party Packages
