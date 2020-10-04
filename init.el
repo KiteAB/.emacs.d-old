38a69125b51414dc5b3072df0ce7680440b7cc5d
David Zhang <kiteabpl@outlook.com>
2020-10-04 11:57:13 +0800

    comments

--- init.el
+++ init.el
@@ -1,12 +1,13 @@
-;;  __  __         _____
-;; |  \/  |_   _  | ____|_ __ ___   __ _  ___ ___
-;; | |\/| | | | | |  _| | '_ ` _ \ / _` |/ __/ __|
-;; | |  | | |_| | | |___| | | | | | (_| | (__\__ \
-;; |_|  |_|\__, | |_____|_| |_| |_|\__,_|\___|___/
-;;         |___/
-
-;; Author: KiteAB (https://github.com/KiteAB)
-;; Repository: https://github.com/KiteAB/.emacs.d
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  _  ___ _         _   ___   ___									  ;;
+;; | |/ (_) |_ ___  /_\ | _ ) | __|_ __  __ _ __ ___  ;;
+;; | ' <| |  _/ -_)/ _ \| _ \ | _|| '  \/ _` / _(_-<  ;;
+;; |_|\_\_|\__\___/_/ \_\___/ |___|_|_|_\__,_\__/__/  ;;
+;; 																									  ;;
+;; Author			: KiteAB (https://github.com/KiteAB)	  ;;
+;; Repository	: https://github.com/KiteAB/.emacs.d	  ;;
+;; E-Mail			: kiteabpl@outlook.com								  ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; Auto opzimization
 (setq gc-cons-threshold-original gc-cons-threshold)
