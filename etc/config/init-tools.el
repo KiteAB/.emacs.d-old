3ba5f863f1be3a163d7d30f991dad7ff18302cdd
David Zhang <kiteabpl@outlook.com>
2020-10-08 13:33:35 +0800

    added whitespace, config like #1

--- etc/config/init-tools.el
+++ etc/config/init-tools.el
@@ -282,4 +282,27 @@
   :defer 1
   :bind (("M-X" . smex)))
 
+;;; WhiteSpace
+(use-package whitespace
+  :ensure nil
+  :defer t
+  :hook
+  ('after-init . 'global-whitespace-mode)
+  :config
+  (setq whitespace-style
+        '(face tabs                     tab-mark
+               space-after-tab::space   space-before-tab::space
+               indentation::space
+               trailing                 empty))
+  (setq whitespace-display-mappings
+        '(;; "tab" char.      Display like "|   ".   Or Display like "\   "
+          (tab-mark      9   [124 9]   [92 9])
+          ;; " " char.        Display like "·".      Or Display like "_"
+          (space-mark    32  [183]     [95])
+          ;; "newline" char.  Display like "¬"       Or Display like "¶"
+          (newline-mark  10  [172 10]  [182 10])))
+  (set-face-attribute 'whitespace-tab      nil :foreground "#444444" :background "#686868")
+  (set-face-attribute 'whitespace-empty    nil :foreground "#cd8c95" :background "#8b5f65")
+  (set-face-attribute 'whitespace-trailing nil :foreground "#79cdcd" :background "#668b8b"))
+
 (provide 'init-tools)
