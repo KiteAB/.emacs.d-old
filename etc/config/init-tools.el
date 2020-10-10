fc5ce3ca05ac23ea174442b45e80b9c8c97e5e75
David Zhang <kiteabpl@outlook.com>
2020-10-10 23:19:11 +0800

    better func

--- etc/config/init-tools.el
+++ etc/config/init-tools.el
@@ -8,7 +8,7 @@
   (eaf-setq eaf-browser-remember-history "true")
   (eaf-setq eaf-browser-default-zoom "1.5")
   (defalias 'browse-web #'eaf-open-browser)
-  (if (and (< 18 (car (circadian-now-time))) (> 8 (car (circadian-now-time))))
+  (if (and (< (car (circadian-sunset)) (car (circadian-now-time))) (> (car (circadian-sunrise)) (car (circadian-now-time))))
       (eaf-setq eaf-browser-dark-mode "true")
     (eaf-setq eaf-browser-dark-mode "false")))
 
