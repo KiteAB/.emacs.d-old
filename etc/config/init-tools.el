31cb08e97fb6db96d45fca9f16ae4b61d65e7bfc
David Zhang <kiteabpl@outlook.com>
2020-10-10 22:10:36 +0800

    added pyim

--- etc/config/init-tools.el
+++ etc/config/init-tools.el
@@ -306,4 +306,21 @@
   :bind (("C-' l r" . rotate-window)
          ("C-' l l" . rotate-layout)))
 
+;;; PYIM
+(use-package pyim
+  :ensure t
+  :bind (("C-SPC" . toggle-input-method))
+  :config
+  (use-package pyim-basedict
+    :ensure t
+    :config (pyim-basedict-enable))
+  (setq default-input-method "pyim"
+        pyim-default-scheme 'quanpin
+        pyim-page-tooltip 'posframe
+        pyim-page-length 5
+        pyim-page-style 'one-line
+        pyim-punctuation-translate-p '(no yes auto)
+        pyim-enable-shortcode nil)
+  (pyim-isearch-mode t))
+
 (provide 'init-tools)
