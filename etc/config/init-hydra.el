86bf28bfcf7ad6eb9c1a2ccdab76e8d96579edc0
David Zhang <kiteabpl@outlook.com>
2020-10-10 18:59:54 +0800

    major mode hydra

--- etc/config/init-hydra.el
+++ etc/config/init-hydra.el
@@ -1,4 +1,5 @@
 ;;; This file is used for Hydra configuration
+;; <TODO(KiteAB)> https://github.com/EvanMeek/.emacs.d/blob/master/etc/config/init-keybind.el [Sat Oct 10 18:59:41 2020]
 (define-prefix-command 'leader-key)
 (which-key-add-key-based-replacements
   )
@@ -9,5 +10,9 @@
 (use-package hydra-posframe
   :load-path "~/.emacs.d/site-lisp/hydra-posframe"
   :hook (after-init-hook . hydra-posframe-mode))
+
+(use-package major-mode-hydra
+  :ensure t
+  :after hydra)
 
 (provide 'init-hydra)
