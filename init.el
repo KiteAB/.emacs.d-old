2b6e51fa41669c7f1b1c958c71b82d8be67676b5
David Zhang <kiteabpl@outlook.com>
2020-10-20 21:26:03 +0800

    comments

--- init.el
+++ init.el
@@ -1,13 +1,37 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;  _  ___ _         _   ___   ___                   ;;
-;; | |/ (_) |_ ___  /_\ | _ ) | __|_ __  __ _ __ ___ ;;
-;; | ' <| |  _/ -_)/ _ \| _ \ | _|| '  \/ _` / _(_-< ;;
-;; |_|\_\_|\__\___/_/ \_\___/ |___|_|_|_\__,_\__/__/ ;;
-;;                                                   ;;
-;; Repository: https://github.com/KiteAB/.emacs.d    ;;
-;; Author: KiteAB <https://github.com/KiteAB>        ;;
-;; E-Mail: <kiteabpl@outlook.com>                    ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; .emacs.d ---  My Personal Emacs Configuration File
+
+;; Author: KiteAB <kiteabpl@outlook.com>
+;; Maintainer: KiteAB <kiteabpl@outlook.com>
+;; Copyright (C) 2020, KiteAB, all rights reserved.
+;; Last-Updated: 2020-10-20 21:25:34
+;;           By: KiteAB
+;; URL: http://github.com/KiteAB/.emacs.d
+;; Keywords:
+;; Compatibility: GNU Emacs 27.0.50
+;;
+;;; This file is NOT part of GNU Emacs
+
+;;; License
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; TODO
+;;
+;;
+;;
 
 ;;; Automatic Optimization
 (setq gc-cons-threshold-original gc-cons-threshold)
@@ -33,11 +57,3 @@
 
 ;;; Require Configuration Files
 (require 'init-config)
-
-;;; Display Startup Time
-(add-hook 'emacs-startup-hook (lambda ()
-                                (message "Emacs started in %s with %d garbage collections."
-                                         (format "%.2f seconds"
-                                                 (float-time
-                                                  (time-subtract after-init-time before-init-time)))
-                                         gcs-done)))
