25baa370e240bf2563f74cd2783338371b5578d0
David Zhang <kiteabpl@outlook.com>
2020-10-21 21:00:52 +0800

    tsinghua source

--- etc/config/init-elpa.el
+++ etc/config/init-elpa.el
@@ -11,8 +11,8 @@
 ;;; Mirror
 (require 'package)
 (require 'cl-lib) ; Common Lisp
-(setq package-archives '(("gnu" . "http://mirrors.bfsu.edu.cn/elpa/gnu/")
-                         ("melpa" . "http://mirrors.bfsu.edu.cn/elpa/melpa/")))
+(setq package-archives '(("gnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 ;;; Init Use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
