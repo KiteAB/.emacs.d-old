81b47de9b5ae63db1b788a6b56f33e2770071537
David Zhang <kiteabpl@outlook.com>
2020-10-25 16:39:38 +0800

    made pyim can autoload

--- lisp/extensions/init-pyim.el
+++ lisp/extensions/init-pyim.el
@@ -2,17 +2,19 @@
 (use-package pyim
   :ensure t
   :bind ("C-SPC" . toggle-input-method)
+  :custom
+  (default-input-method 'pyim)
+  (pyim-default-scheme 'quanpin)
+  (pyim-page-tooltip 'posframe)
+  (pyim-page-length 5)
+  (pyim-page-style 'one-line)
+  (pyim-punctuation-translate-p '(no yes auto))
+  (pyim-enable-shortcode nil)
   :config
   (use-package pyim-basedict
     :ensure t
     :config (pyim-basedict-enable))
-  (setq default-input-method 'pyim
-        pyim-default-scheme 'quanpin
-        pyim-page-tooltip 'posframe
-        pyim-page-length 5
-        pyim-page-style 'one-line
-        pyim-punctuation-translate-p '(no yes auto)
-        pyim-enable-shortcode nil)
-  (pyim-isearch-mode t))
+  (pyim-isearch-mode t)
+  (message ""))
 
 (provide 'init-pyim)
