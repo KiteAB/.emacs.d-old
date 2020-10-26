5210fa83333d45ba00e12b75a6497a044e873d36
David Zhang <kiteabpl@outlook.com>
2020-10-26 22:00:15 +0800

    better code style

--- site-lisp/config/init-awesome-tray.el
+++ site-lisp/config/init-awesome-tray.el
@@ -10,16 +10,25 @@
   (add-to-list 'awesome-tray-module-alist '("current-input-method" . (kiteab/awetray-current-input-method awesome-tray-module-battery-face)))
 
   (defun kiteab/awetray-buffer-read-only ()
-    (if (and (eq buffer-read-only t) (not (eq buffer-file-name nil)))
+    (if (and (eq buffer-read-only t)
+             (not (eq buffer-file-name nil)))
         "R-O"))
   (add-to-list 'awesome-tray-module-alist '("buffer-read-only" . (kiteab/awetray-buffer-read-only awesome-tray-module-git-face)))
 
   (defun kiteab/awetray-buffer-name ()
-    (if (and (buffer-modified-p) (not (eq buffer-file-name nil)))
+    (if (and (buffer-modified-p)
+             (not (eq buffer-file-name nil)))
         (concat (buffer-name) "*")
       (buffer-name)))
   (add-to-list 'awesome-tray-module-alist '("buffer-name-plus" . (kiteab/awetray-buffer-name awesome-tray-module-buffer-name-face)))
 
-  (setq awesome-tray-active-modules '("git" "location" "current-input-method" "mode-name" "parent-dir" "buffer-name-plus" "buffer-read-only" "date")))
+  (setq awesome-tray-active-modules '("git"
+                                      "location"
+                                      "current-input-method"
+                                      "mode-name"
+                                      "parent-dir"
+                                      "buffer-name-plus"
+                                      "buffer-read-only"
+                                      "date")))
 
 (provide 'init-awesome-tray)
