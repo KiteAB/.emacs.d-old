785d6b867e1fa2d5657d7693bfc1f483f1ae21cd
David Zhang <kiteabpl@outlook.com>
2020-10-27 22:31:44 +0800

    merged early-init.el and init.el

--- init.el
+++ init.el
@@ -33,6 +33,18 @@
 ;;
 ;;
 
+;;; Automatic Optimization
+(setq gc-cons-threshold-original gc-cons-threshold)
+(setq gc-cons-threshold (* 1024 1024 100))
+(setq file-name-handler-alist-original file-name-handler-alist)
+(setq inhibit-compacting-font-caches nil)
+(setq file-name-handler-alist nil)
+(run-with-idle-timer 5 nil (lambda ()
+                             (setq gc-cons-threshold gc-cons-threshold-original)
+                             (setq file-name-handler-alist file-name-handler-alist-original)
+                             (makunbound 'gc-cons-threshold-original)
+                             (makunbound 'file-name-handler-alist-original)))
+
 ;;; Traverse Load Configuration Folder
 (defun add-subdirs-to-load-path(dir)
   "Recursive add directories to `load-path`."
