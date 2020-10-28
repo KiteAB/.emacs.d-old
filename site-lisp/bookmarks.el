524a4daf5b4e65140b07a788a03044eced3dc8a7
David Zhang <kiteabpl@outlook.com>
2020-10-28 22:51:22 +0800

    added bookmarks for eaf

---
+++ site-lisp/bookmarks.el
@@ -0,0 +1,9 @@
+;;;; Emacs Bookmark Format Version 1 ;;;; -*- coding: utf-8-emacs -*-
+;;; This format is meant to be slightly human-readable;
+;;; nevertheless, you probably don't want to edit it.
+;;; -*- End Of Bookmark File Format Version Stamp -*-
+(("Dynamic of Bilibili"
+ (handler . eaf--bookmark-restore)
+ (eaf-app . "browser")
+ (filename . "https://t.bilibili.com/"))
+)
