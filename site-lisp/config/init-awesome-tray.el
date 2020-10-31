649f44a6f4ff172f1b3fc772aee4d3f05c99cec2
David Zhang <kiteabpl@outlook.com>
2020-10-31 17:24:30 +0800

    remove all modules definitions

--- site-lisp/config/init-awesome-tray.el
+++ site-lisp/config/init-awesome-tray.el
@@ -3,33 +3,16 @@
   :load-path "~/.emacs.d/site-lisp/extensions/awesome-tray"
   :hook (after-init-hook . awesome-tray-mode)
   :config
-  (defun kiteab/awetray-current-input-method ()
-    (if (eq current-input-method nil)
-        "EN"
-      "ZH"))
-  (add-to-list 'awesome-tray-module-alist '("current-input-method" . (kiteab/awetray-current-input-method awesome-tray-module-battery-face)))
-
-  (defun kiteab/awetray-buffer-read-only ()
-    (if (and (eq buffer-read-only t)
-             (not (eq buffer-file-name nil)))
-        "R-O"))
-  (add-to-list 'awesome-tray-module-alist '("buffer-read-only" . (kiteab/awetray-buffer-read-only awesome-tray-module-git-face)))
-
-  (defun kiteab/awetray-buffer-name ()
-    (if (and (buffer-modified-p)
-             (not (eq buffer-file-name nil)))
-        (concat (buffer-name) "*")
-      (buffer-name)))
-  (add-to-list 'awesome-tray-module-alist '("buffer-name-plus" . (kiteab/awetray-buffer-name awesome-tray-module-buffer-name-face)))
+  (setq awesome-tray-refresh-idle-delay 0.01
+        awesome-tray-buffer-name-buffer-changed t)
 
   (setq awesome-tray-active-modules '("git"
                                       "location"
-                                      "current-input-method"
+                                      "input-method"
                                       "mode-name"
                                       "parent-dir"
-                                      "buffer-name-plus"
+                                      "buffer-name"
                                       "buffer-read-only"
-                                      "date"))
-  (setq awesome-tray-refresh-idle-delay 0.01))
+                                      "date")))
 
 (provide 'init-awesome-tray)
