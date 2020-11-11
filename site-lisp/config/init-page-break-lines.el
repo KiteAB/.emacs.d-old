f9ec5deddb89ecc96240353109571012bdf00d86
David Zhang <kiteabpl@outlook.com>
2020-11-11 17:40:40 +0800

    removed page break lines

--- site-lisp/config/init-page-break-lines.el
+++
@@ -1,6 +0,0 @@
-;;; This file is used for Page Break Lines Settings
-(use-package page-break-lines
-  :ensure t
-  :hook (prog-mode-hook . page-break-lines-mode))
-
-(provide 'init-page-break-lines)
