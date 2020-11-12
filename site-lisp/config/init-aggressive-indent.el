797bb40b5a8e517ba4c650391322dad8f0705bf9
David Zhang <kiteabpl@outlook.com>
2020-11-12 18:04:37 +0800

    updated

--- site-lisp/config/init-aggressive-indent.el
+++ site-lisp/config/init-aggressive-indent.el
@@ -1,6 +1,7 @@
 ;;; This file is used for Aggressive Indent Settings
 (use-package aggressive-indent
   :ensure t
-  :hook (prog-mode-hook . aggressive-indent-mode))
+  ;; :hook (prog-mode-hook . aggressive-indent-mode)
+  )
 
 (provide 'init-aggressive-indent)
