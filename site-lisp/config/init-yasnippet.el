d340fe3b36e62718c897a94cdca960ab876555cb
David Zhang <kiteabpl@outlook.com>
2020-11-14 18:58:43 +0800

    updated

--- site-lisp/config/init-yasnippet.el
+++ site-lisp/config/init-yasnippet.el
@@ -1,13 +1,14 @@
 ;;; init-yasnippet.el --- Settings for Yasnippet
 ;;; Code:
+(leaf yasnippet-snippets :ensure t)
 (leaf yasnippet
   :ensure t
+  :disabled t
   :bind
   ("C-' C-y" . yas-expand-from-trigger-key)
   :hook after-init-hook
   :config
   (yas-global-mode)
-  (leaf yasnippet-snippets :ensure t)
   :custom
   (yas-snippet-dirs . '("~/.emacs.d/site-lisp/snippets"
                         "~/.emacs.d/elpa/yasnippet-snippets-20201025.1809/snippets")))
