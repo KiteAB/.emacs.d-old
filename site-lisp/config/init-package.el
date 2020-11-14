b6c5f06ea7a0874b2f934ffdc83a88de3114e485
David Zhang <kiteabpl@outlook.com>
2020-11-14 19:08:21 +0800

    Revert "updated"
    This reverts commit d340fe3b36e62718c897a94cdca960ab876555cb.

--- site-lisp/config/init-package.el
+++ site-lisp/config/init-package.el
@@ -10,10 +10,10 @@
 (setq package-archives '(("gnu" . "http://mirrors.bfsu.edu.cn/elpa/gnu/")
                          ("melpa" . "http://mirrors.bfsu.edu.cn/elpa/melpa/")))
 ;;; Init Use Package
-;; (unless (package-installed-p 'use-package)
-;;   (package-refresh-contents)
-;;   (package-install 'use-package))
-;; (setq use-package-hook-name-suffix nil)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(setq use-package-hook-name-suffix nil)
 
 ;;; Init Leaf.el
 (unless (and (package-installed-p 'leaf) (package-installed-p 'leaf-keywords))
