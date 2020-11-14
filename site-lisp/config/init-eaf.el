d42075929f5835ad05726c2661b162346641b522
David Zhang <kiteabpl@outlook.com>
2020-11-14 12:41:12 +0800

    eaf font

--- site-lisp/config/init-eaf.el
+++ site-lisp/config/init-eaf.el
@@ -6,6 +6,7 @@
   (setq eaf-grip-token "c9a51aa390cd3bce36a7d307d404d2aeb030f707")
   (eaf-setq eaf-browser-remember-history "true")
   (eaf-setq eaf-browser-default-zoom "1.5")
+  (eaf-setq eaf-terminal-font-family "Sarasa Mono Slab SC Semibold")
   (setq eaf-browser-search-engines '(("Baidu" . "https://www.baidu.com/s?wd=%s")
                                      ("Bing" . "https://cn.bing.com/search?q=%s")))
   (setq eaf-browser-default-search-engine "Bing")
