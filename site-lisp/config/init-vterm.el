2dca692b18789cadb770746004595e0bb6fb2dba
David Zhang <kiteabpl@outlook.com>
2020-11-19 17:55:26 +0800

    disabled vterm

--- site-lisp/config/init-vterm.el
+++ site-lisp/config/init-vterm.el
@@ -1,6 +1,7 @@
 ;;; This file is used for VTerm Settings
 (use-package vterm
   :ensure t
+  :disabled
   :config
   (define-key vterm-mode-map (kbd "C-c p") 'previous-buffer)
   (define-key vterm-mode-map (kbd "C-c n") 'next-buffer))
