b0a39d1b13ee10ee01f4e32e2f4218ca7dc6b4b4
David Zhang <kiteabpl@outlook.com>
2020-11-19 20:21:15 +0800

    accelerate

--- site-lisp/config/init-accelerate.el
+++ site-lisp/config/init-accelerate.el
@@ -1,6 +1,7 @@
 ;;; init-accelerate.el --- Accelerate for startup
 ;;; Code:
-(setq package-enable-at-startup nil)
+(setq package-enable-at-startup nil
+      frame-inhibit-implied-resize t)
 
 (provide 'init-accelerate)
 
