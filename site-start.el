698832ca72e417bb678598a7d9cc9783d172e66a
David Zhang <kiteabpl@outlook.com>
2020-12-01 17:25:25 +0800

    fix(site-start.el): Updated Last-Updated time

--- site-start.el
+++ site-start.el
@@ -3,7 +3,7 @@
 ;; Author: KiteAB <kiteabpl@outlook.com>
 ;; Maintainer: KiteAB <kiteabpl@outlook.com>
 ;; Copyright (C) 2020, KiteAB, all rights reserved.
-;; Last-Updated: 2020-11-28 17:17:00
+;; Last-Updated: 2020-12-01 17:24:09
 ;;           By: KiteAB
 ;; URL: http://github.com/KiteAB/.emacs.d
 ;; Keywords:
