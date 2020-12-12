50bfc93598d932e6bf7d368d61d5cbb12378d228
David Zhang <kiteabpl@outlook.com>
2020-12-12 21:34:32 +0800

    feat(init-xah-fly-keys.el): Use xah fly keys instead evil

---
+++ site-lisp/config/init-xah-fly-keys.el
@@ -0,0 +1,11 @@
+;;; init-xah-fly-keys.el --- Settings for xah fly keys
+;;; Code:
+(use-package xah-fly-keys
+  :ensure t
+  ;; :hook (after-init-hook . xah-fly-keys)
+  :config
+  (xah-fly-keys-set-layout "colemak"))
+
+(provide 'init-xah-fly-keys)
+
+;;; init-xah-fly-keys.el ends here
