2c52bfa36e32d71fa004efbb881704e42ee968de
David Zhang <kiteabpl@outlook.com>
2020-12-12 21:48:07 +0800

    feat(README.org init-xah-fly-keys.el): README & Start using xah fly keys

--- site-lisp/config/init-xah-fly-keys.el
+++ site-lisp/config/init-xah-fly-keys.el
@@ -2,7 +2,7 @@
 ;;; Code:
 (use-package xah-fly-keys
   :ensure t
-  ;; :hook (after-init-hook . xah-fly-keys)
+  :hook (after-init-hook . xah-fly-keys)
   :config
   (xah-fly-keys-set-layout "colemak"))
 
