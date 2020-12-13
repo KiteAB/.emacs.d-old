3f404db6b97397fcff516773597ce93d5285b39c
David Zhang <kiteabpl@outlook.com>
2020-12-13 19:25:47 +0800

    feat(init-xah-fly-keys.el): Basic usage

--- site-lisp/config/init-xah-fly-keys.el
+++ site-lisp/config/init-xah-fly-keys.el
@@ -5,7 +5,20 @@
   :hook (after-init-hook . xah-fly-keys)
   :config
   (xah-fly-keys-set-layout "colemak")
-  (define-key xah-fly-insert-map [escape] #'xah-fly-command-mode-activate))
+  (add-hook 'magit-mode-hook #'(lambda () (interactive) (xah-fly-keys -1)))
+  (add-hook 'minibuffer-inactive-hook #'(lambda () ) (interactive) (xah-fly-keys 1))
+  (define-key xah-fly-insert-map [escape]     #'xah-fly-command-mode-activate)
+  (define-key xah-fly-command-map (kbd "U")   #'(lambda () (interactive) (previous-line 5)))
+  (define-key xah-fly-command-map (kbd "E")   #'(lambda () (interactive) (next-line 5)))
+  (define-key xah-fly-command-map (kbd "N")   #'(lambda () (interactive) (beginning-of-line)))
+  (define-key xah-fly-command-map (kbd "I")   #'(lambda () (interactive) (end-of-line)))
+  (define-key xah-fly-command-map (kbd "C-n") #'(lambda () (interactive) (next-line)))
+  (define-key xah-fly-command-map (kbd "C-a") #'(lambda () (interactive) (beginning-of-line)))
+  (define-key xah-fly-command-map (kbd "S")   #'(lambda () (interactive) (save-buffer)))
+  (define-key xah-fly-command-map (kbd "Q")   #'(lambda () (interactive) (kill-current-buffer)))
+  (define-key xah-fly-command-map (kbd "A")   #'(lambda () (interactive) (end-of-line) (xah-fly-insert-mode-activate)))
+  (define-key xah-fly-command-map (kbd "K")   #'(lambda () (interactive) (beginning-of-line) (xah-fly-insert-mode-activate)))
+  (define-key xah-fly-command-map (kbd "k")   #'(lambda () (interactive) (xah-fly-insert-mode-activate))))
 
 (provide 'init-xah-fly-keys)
 
