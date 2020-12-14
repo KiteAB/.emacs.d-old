be51e41755223e4286e0fc3fdc3dfa8431f47f6b
David Zhang <kiteabpl@outlook.com>
2020-12-14 20:50:53 +0800

    style(init-xah-fly-keys.el): More standard codes

--- site-lisp/config/init-xah-fly-keys.el
+++ site-lisp/config/init-xah-fly-keys.el
@@ -2,26 +2,57 @@
 ;;; Code:
 (use-package xah-fly-keys
   :ensure t
-  :hook (after-init-hook . xah-fly-keys)
-  :config
-  (xah-fly-keys-set-layout "colemak")
+  :demand
+  :hook (after-init-hook . xah-fly-keys))
 
-  (add-hook 'magit-mode-hook #'(lambda () (interactive) (xah-fly-keys -1)))
+(defun xfk-define-in-all-modes (key def)
+  (define-key xah-fly-command-map (kbd key) def)
+  (define-key xah-fly-insert-map (kbd key) def))
 
-  (define-key xah-fly-insert-map  [escape]     #'xah-fly-command-mode-activate)
-  (define-key xah-fly-insert-map  (kbd "C-n") #'(lambda () (interactive) (next-line)))
+(defun xfk-last-buffer (arg)
+  (interactive "P")
+  (if (not arg)
+      (mode-line-other-buffer)
+    (split-window)
+    (mode-line-other-buffer)))
 
-  (define-key xah-fly-command-map (kbd "U")   #'(lambda () (interactive) (previous-line 5)))
-  (define-key xah-fly-command-map (kbd "E")   #'(lambda () (interactive) (next-line 5)))
-  (define-key xah-fly-command-map (kbd "N")   #'(lambda () (interactive) (beginning-of-line)))
-  (define-key xah-fly-command-map (kbd "I")   #'(lambda () (interactive) (end-of-line)))
-  (define-key xah-fly-command-map (kbd "C-n") #'(lambda () (interactive) (next-line)))
-  (define-key xah-fly-command-map (kbd "C-a") #'(lambda () (interactive) (beginning-of-line)))
-  (define-key xah-fly-command-map (kbd "S")   #'(lambda () (interactive) (save-buffer)))
-  (define-key xah-fly-command-map (kbd "Q")   #'(lambda () (interactive) (kill-current-buffer)))
-  (define-key xah-fly-command-map (kbd "A")   #'(lambda () (interactive) (end-of-line) (xah-fly-insert-mode-activate)))
-  (define-key xah-fly-command-map (kbd "K")   #'(lambda () (interactive) (beginning-of-line) (xah-fly-insert-mode-activate)))
-  (define-key xah-fly-command-map (kbd "k")   #'(lambda () (interactive) (xah-fly-insert-mode-activate))))
+(defun xfk-up-5-lines ()
+  (interactive)
+  (previous-line 5))
+
+(defun xfk-down-5-lines ()
+  (interactive)
+  (next-line 5))
+
+(defun xfk-insert-at-beginning ()
+  (interactive)
+  (beginning-of-line)
+  (xah-fly-insert-mode-activate))
+
+(defun xfk-insert-at-end ()
+  (interactive)
+  (end-of-line)
+  (xah-fly-insert-mode-activate))
+
+(xah-fly-keys-set-layout "colemak")
+
+(add-hook 'magit-mode-hook #'(lambda () (interactive) (xah-fly-keys -1)))
+
+(xfk-define-in-all-modes "<f11>" #'toggle-frame-fullscreen)
+(xfk-define-in-all-modes "C-n" #'next-line)
+(xfk-define-in-all-modes "C-a" #'beginning-of-line)
+
+(define-key xah-fly-insert-map  [escape]    #'xah-fly-command-mode-activate)
+(define-key xah-fly-command-map [escape]    #'xfk-last-buffer)
+(define-key xah-fly-command-map (kbd "N")   #'beginning-of-line)
+(define-key xah-fly-command-map (kbd "I")   #'end-of-line)
+(define-key xah-fly-command-map (kbd "k")   #'xah-fly-insert-mode-activate)
+(define-key xah-fly-command-map (kbd "S")   #'save-buffer)
+(define-key xah-fly-command-map (kbd "Q")   #'kill-current-buffer)
+(define-key xah-fly-command-map (kbd "U")   #'xfk-up-5-lines)
+(define-key xah-fly-command-map (kbd "E")   #'xfk-down-5-lines)
+(define-key xah-fly-command-map (kbd "K")   #'xfk-insert-at-beginning)
+(define-key xah-fly-command-map (kbd "A")   #'xfk-insert-at-end)
 
 (provide 'init-xah-fly-keys)
 
