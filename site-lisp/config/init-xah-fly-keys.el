8a6c7cc0ff4d6064684ee9f52ed45f2e563c7bc0
David Zhang <kiteabpl@outlook.com>
2020-12-15 19:04:18 +0800

    fix(init-xah-fly-keys.el): Fix bugs

--- site-lisp/config/init-xah-fly-keys.el
+++ site-lisp/config/init-xah-fly-keys.el
@@ -8,13 +8,13 @@
   (define-key xah-fly-command-map (kbd key) def)
   (define-key xah-fly-insert-map  (kbd key) def))
 
-(defun xfk-last-buffer (arg)
-  (interactive "P")
-  (if (not arg)
-      (mode-line-other-buffer)
-    (split-window)
-    (mode-line-other-buffer)))
+(defun xfk-define-command-key (key func)
+  (define-key xah-fly-command-map (kbd key) func))
 
+(defun xfk-define-insert-key (key func)
+  (define-key xah-fly-insert-map (kbd key) func))
+
+;; Motions
 (defun xfk-up-5-lines ()
   (interactive)
   (previous-line 5))
@@ -33,14 +33,24 @@
   (end-of-line)
   (xah-fly-insert-mode-activate))
 
+(defun xfk-forward-char ()
+  (interactive)
+  (when (< (point) (line-end-position))
+    (forward-char (abs (prefix-numeric-value nil)))))
+
+(defun xfk-backward-char ()
+  (interactive)
+  (when (> (point) (line-beginning-position))
+    (backward-char (abs (prefix-numeric-value nil)))))
+
 (defun xfk-forward-char-and-insert ()
   (interactive)
-  (forward-char)
+  (xfk-forward-char)
   (xah-fly-insert-mode-activate))
 
 (defun xfk-backward-char-and-command ()
   (interactive)
-  (backward-char)
+  (xfk-backward-char)
   (xah-fly-command-mode-activate))
 
 (xah-fly-keys-set-layout "colemak")
@@ -48,21 +58,31 @@
 (add-hook 'magit-mode-hook #'(lambda () (interactive) (xah-fly-keys -1)))
 
 (xfk-define-in-all-modes "<f11>" #'toggle-frame-fullscreen)
-(xfk-define-in-all-modes "C-n" #'next-line)
-(xfk-define-in-all-modes "C-a" #'beginning-of-line)
+(xfk-define-in-all-modes "C-n"   #'next-line)
+(xfk-define-in-all-modes "C-a"   #'beginning-of-line)
 
-(define-key xah-fly-insert-map  [escape]    #'xfk-backward-char-and-command)
-(define-key xah-fly-command-map [escape]    #'xfk-last-buffer)
-(define-key xah-fly-command-map (kbd "N")   #'beginning-of-line)
-(define-key xah-fly-command-map (kbd "I")   #'end-of-line)
-(define-key xah-fly-command-map (kbd "k")   #'xah-fly-insert-mode-activate)
-(define-key xah-fly-command-map (kbd "S")   #'save-buffer)
-(define-key xah-fly-command-map (kbd "Q")   #'kill-current-buffer)
-(define-key xah-fly-command-map (kbd "U")   #'xfk-up-5-lines)
-(define-key xah-fly-command-map (kbd "E")   #'xfk-down-5-lines)
-(define-key xah-fly-command-map (kbd "K")   #'xfk-insert-at-beginning)
-(define-key xah-fly-command-map (kbd "A")   #'xfk-insert-at-end)
-(define-key xah-fly-command-map (kbd "a")   #'xfk-forward-char-and-insert)
+(define-key xah-fly-insert-map  [escape] #'xfk-backward-char-and-command)
+(define-key xah-fly-command-map [escape] #'mode-line-other-buffer)
+
+(xfk-define-command-key "t"     nil)
+(xfk-define-command-key "M-SPC" nil)
+
+(xfk-define-command-key "n" #'xfk-backward-char)
+(xfk-define-command-key "i" #'xfk-forward-char)
+
+(xfk-define-command-key "N" #'beginning-of-line)
+(xfk-define-command-key "I" #'end-of-line)
+(xfk-define-command-key "k" #'xah-fly-insert-mode-activate)
+(xfk-define-command-key "S" #'save-buffer)
+(xfk-define-command-key "Q" #'kill-current-buffer)
+(xfk-define-command-key "U" #'xfk-up-5-lines)
+(xfk-define-command-key "E" #'xfk-down-5-lines)
+(xfk-define-command-key "K" #'xfk-insert-at-beginning)
+(xfk-define-command-key "A" #'xfk-insert-at-end)
+(xfk-define-command-key "a" #'xfk-forward-char-and-insert)
+(xfk-define-command-key "v" #'set-mark-command)
+
+(xfk-define-command-key "?" 'help-command)
 
 (provide 'init-xah-fly-keys)
 
