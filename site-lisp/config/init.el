26ac488421bced3fa12a9041fb84f87e474215f7
David Zhang <kiteabpl@outlook.com>
2020-12-17 17:48:52 +0800

    feat(init.el): Re-make init.el

--- site-lisp/config/init.el
+++ site-lisp/config/init.el
@@ -1,5 +1,6 @@
 ;;; init.el --- Require features
 ;;; Code:
+;; Preparation
 (require 'init-accelerate)
 (require 'init-font)
 
@@ -8,73 +9,88 @@
       (gc-cons-percentage 0.6)
       (file-name-handler-alist nil))
   (with-temp-message ""
+    ;; Basic
     (require 'init-generic)
-    (require 'init-ui)
-    (require 'init-functions)
     (require 'init-package)
-    (require 'init-theme)
-    (require 'init-awesome-tray)
+    (require 'init-macros)
+    (require 'init-modes)
+    (require 'init-functions)
 
+    ;; Basic User Interface
+    (require 'init-ui)
+    (require 'init-awesome-tray)
+    (require 'init-theme)
+
+    ;; Keybinding
     (require 'init-keybind)
     (require 'init-xfk)
 
+    ;; Package
+    ;; Org
+    (require 'init-org)
+
+    ;; Emacs Lisp
+    (require 'init-awesome-pair)
+    (require 'init-symbol-overlay)
+    (require 'init-highlight-defined)
+    (require 'init-highlight-quoted)
+    ;; (require 'init-rainbow-delimiters)
+    ;; (require 'init-lispy)
+
+    ;; Auto Complete
     (require 'init-company-mode)
     (require 'init-lsp)
     (require 'init-elpy)
+
+    ;; Project
     (require 'init-projectile)
-    (require 'init-symbol-overlay)
-    (require 'init-awesome-pair)
-    ;; (require 'init-lispy)
-    (require 'init-paren-face)
-    (require 'init-rainbow-delimiters)
-    (require 'init-highlight-defined)
-    (require 'init-highlight-quoted)
-    (require 'init-auto-save)
-    (require 'init-flycheck)
-    (require 'init-vterm)
-    (require 'init-counsel)
-    (require 'init-which-key)
-    (require 'init-auto-sudoedit)
-    (require 'init-ace-window)
-    (require 'init-windresize)
-    ;; (require 'init-pyim)
-    (require 'init-smooth-scrolling)
-    (require 'init-multiple-cursors)
-    (require 'init-clm)
-    (require 'init-isearch)
-    (require 'init-comment)
-    (require 'init-evil-nerd-commenter)
-    (require 'init-winner-mode)
-    (require 'init-recentf)
-    (require 'init-acejump)
-    (require 'init-disable-mouse)
-    (require 'init-popwin)
-    (require 'init-git)
     (require 'init-treemacs)
-    (require 'init-yasnippet)
-    (require 'init-ivy)
-    (require 'init-whitespace)
-    (require 'init-eaf)
-    (require 'init-web-mode)
-    (require 'init-yaml-mode)
-    (require 'init-aggressive-indent)
-    (require 'init-indent-guide)
-    (require 'init-info-colors)
-    (require 'init-dired)
-    (require 'init-rainbow-mode)
-    (require 'init-company-english-helper)
-    (require 'init-insert-translated-name)
-    (require 'init-youdao-dictionary)
-    (require 'init-pacarch)
-    (require 'init-org)
 
-    (require 'init-modes)
-    (require 'init-macros)
-
-    (require 'init-python)
+    ;; Programming
     (require 'init-cc)
     (require 'init-golang)
+    (require 'init-python)
+    (require 'init-web-mode)
+    (require 'init-yaml-mode)
 
+    ;; Tool
+    (require 'init-ace-jump)
+    (require 'init-ace-window)
+    (require 'init-aggressive-indent)
+    (require 'init-auto-save)
+    (require 'init-multiple-cursors)
+    (require 'init-auto-sudoedit)
+    (require 'init-nerd-commenter)
+    (require 'init-pacarch)
+    (require 'init-clm)
+    (require 'init-paren-face)
+    (require 'init-comment)
+    (require 'init-company-eh)
+    (require 'init-counsel)
+    (require 'init-dired)
+    (require 'init-disable-mouse)
+    (require 'init-eaf)
+    (require 'init-flycheck)
+    (require 'init-git)
+    (require 'init-indent-guide)
+    (require 'init-info-colors)
+    (require 'init-insert-translated-name)
+    (require 'init-isearch)
+    (require 'init-ivy)
+    (require 'init-license-templates)
+    (require 'init-popwin)
+    (require 'init-rainbow-mode)
+    (require 'init-recentf)
+    (require 'init-smooth-scrolling)
+    (require 'init-which-key)
+    (require 'init-whitespace)
+    (require 'init-windresize)
+    (require 'init-winner-mode)
+    (require 'init-yasnippet)
+    (require 'init-youdao-dict)
+    ;; (require 'init-vterm)
+
+    ;; Scratch
     (require 'init-scratch)
     ))
 
