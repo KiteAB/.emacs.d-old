df1655de1c98333ba658976f971c0501121fbe1f
David Zhang <kiteabpl@outlook.com>
2020-12-17 21:42:31 +0800

    fix(init-keybind.el): Save session

--- site-lisp/config/init-keybind.el
+++ site-lisp/config/init-keybind.el
@@ -19,6 +19,7 @@
 (global-set-key (kbd "C-q I")     #'kiteab/indent-one-time)
 (global-set-key (kbd "C-q t")     #'kiteab/insert-current-date-time)
 (global-set-key (kbd "C-q SPC")   #'kiteab/format-commit)
+(global-set-key (kbd "C-x C-c")   #'emacs-session-save)
 
 ;;; Built-in Functions -> Keybindings
 (global-set-key (kbd "C-q C-p")   #'previous-buffer)
