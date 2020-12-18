dd29fdccb87e37c00ea123c52d850f86919571c4
David Zhang <kiteabpl@outlook.com>
2020-12-18 20:23:48 +0800

    rename(init-function.el init.el): Renamed init-functions

--- site-lisp/config/init-function.el
+++ site-lisp/config/init-function.el
@@ -1,5 +1,59 @@
-;;; init-functions.el --- Settings for functions
+;;; init-function.el --- Settings for functions
+
+;; Filename: init-function.el
+;; Description: Settings for functions
+;; Author: KiteAB <kiteabpl@outlook.com> (https://kiteab.ga)
+;; Maintainer: KiteAB <kiteabpl@outlook.com> (https://kiteab.ga)
+;; Copyright (C) 2020, KiteAB, all rights reserved.
+;; Created: 2020-12-10 20:59:12
+;; Last-Updated: 2020-12-18 20:12:39
+;;           By: KiteAB
+;; URL: https://github.com/KiteAB/.emacs.d/blob/master/site-lisp/config/init-function.el
+;; Keywords:
+;;
+;; Feature that might be required by this library:
+;;
+;;
+;;
+
+;;; This file is NOT part of GNU Emacs
+
+;;; License
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary
+;;
+;; Settings for functions
+;;
+
+;;; Acknowledgements:
+;;
+;;
+;;
+
+;;; TODO
+;;
+;;
+;;
+
+;;; Require
+
 ;;; Code:
+
 (defun open-config-folder ()
   "Open the config file in the site-lisp/config folder."
   (interactive)
@@ -133,6 +187,6 @@
                   kiteab/git-commit-content))
   (message "[KiteAB Emacs] Action done."))
 
-(provide 'init-functions)
+(provide 'init-function)
 
-;;; init-functions.el ends here
+;;; init-function.el ends here
--- site-lisp/config/init.el
+++ site-lisp/config/init.el
@@ -1,5 +1,57 @@
 ;;; init.el --- Require features
+
+;; Filename: init.el
+;; Description: Require features
+;; Author: KiteAB <kiteabpl@outlook.com> (https://kiteab.ga)
+;; Maintainer: KiteAB <kiteabpl@outlook.com> (https://kiteab.ga)
+;; Copyright (C) 2020, KiteAB, all rights reserved.
+;; Created: 2020-12-10 21:43:50
+;; Last-Updated: 2020-12-18 19:55:23
+;;           By: KiteAB
+;; URL: https://github.com/KiteAB/.emacs.d/blob/master/site-lisp/config/init.el
+;; Keywords:
+;;
+;; Features that might be required by this library:
+;;
+;;
+;;
+
+;;; This file is NOT part of GNU Emacs
+
+;;; License
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;
+;; Require features
+;;
+
+;;; Acknowledgements:
+;;
+;;
+;;
+
+;;; TODO
+;;
+;;
+;;
+
 ;;; Code:
+
 ;; Preparation
 (require 'init-accelerate)
 (require 'init-font)
@@ -12,7 +64,6 @@
     ;; Basic
     (require 'init-generic)
     (require 'init-package)
-    (require 'init-macros)
     (require 'init-time)
     (require 'init-modes)
     (require 'init-functions)
@@ -23,7 +74,7 @@
     (require 'init-theme)
 
     ;; Keybinding
-    (require 'init-keybind)
+    (require 'init-key)
     (require 'init-xfk)
 
     ;; Package
