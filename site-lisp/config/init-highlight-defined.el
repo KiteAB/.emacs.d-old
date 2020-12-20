fb2cf38652ad9f42e04a1badbe11aa2c082175d9
David Zhang <kiteabpl@outlook.com>
2020-12-20 17:14:14 +0800

    feat(...)

--- site-lisp/config/init-highlight-defined.el
+++ site-lisp/config/init-highlight-defined.el
@@ -1,10 +1,65 @@
 ;;; init-highlight-defined.el --- Settings for highlight defined
+
+;; Filename: init-highlight-defined.el
+;; Description: Settings for highlight defined
+;; Author: KiteAB <kiteabpl@outlook.com> (https://kiteab.ga)
+;; Maintainer: KiteAB <kiteabpl@outlook.com> (https://kiteab.ga)
+;; Copyright (C) 2020, KiteAB, all rights reserved.
+;; Created: 2020-12-10 20:59:12
+;; Last-Updated: 2020-12-20 17:09:30
+;;           By: KiteAB
+;; URL: https://github.com/KiteAB/.emacs.d/blob/master/site-lisp/config/init-highlight-defined.el
+;; Keywords:
+;;
+;; Feature that might be required by this library:
+;;
+;;
+;;
+
+;;; This file is NOT part of GNU Emacs
+
+;;; License
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary
+;;
+;; Settings for highlight defined
+;;
+
+;;; Acknowledgements:
+;;
+;;
+;;
+
+;;; TODO
+;;
+;;
+;;
+
+;;; Require
+
 ;;; Code:
-(use-package highlight-defined
-  :ensure t
-  :hook (emacs-lisp-mode-hook . highlight-defined-mode)
-  :config
-  (set-face-attribute 'highlight-defined-function-name-face nil :foreground "Blue"))
+
+(install-pkg 'highlight-defined)
+(add-hook 'emacs-lisp-mode-hook #'(lambda ()
+                                    (require 'highlight-defined)
+                                    (highlight-defined-mode 1)))
+
+(set-face-attribute 'highlight-defined-function-name-face nil :foreground "Blue")
 
 (provide 'init-highlight-defined)
 
