215bf8a7970279d7f0de07e5a7d75b341104cd0f
David Zhang <kiteabpl@outlook.com>
2021-02-03 17:38:14 +0800

    Added yascroll

---
+++ site-lisp/config/init-yascroll.el
@@ -0,0 +1,62 @@
+;;; init-yascroll.el --- Settings for yascroll  -*- lexical-binding: t -*-
+
+;; Filename: init-yascroll.el
+;; Description: Settings for yascroll
+;; Author: KiteAB <kiteabpl@outlook.com> (https://kiteab.ga)
+;; Maintainer: KiteAB <kiteabpl@outlook.com> (https://kiteab.ga)
+;; Copyright (C) 2021, KiteAB, all rights reserved.
+;; Created: 2021-02-03 17:35:13
+;; Last-Updated: 2021-02-03 17:35:13
+;;           By: KiteAB
+;; URL: https://github.com/KiteAB/.emacs.d/blob/master/site-lisp/config/init-yascroll.el
+;; Keywords:
+;;
+;; Features that might be required by this library:
+;;
+;;
+;;
+
+;;; This file is NOT part of GNU Emacs
+
+;;; License
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;
+;; Settings for yascroll
+;;
+
+;;; Acknowledgements:
+;;
+;;
+;;
+
+;;; TODO
+;;
+;;
+;;
+
+;;; Require
+(require-pkg 'yascroll)
+
+;;; Code:
+
+(global-yascroll-bar-mode 1)
+
+(provide 'init-yascroll)
+
+;;; init-yascroll.el ends here
