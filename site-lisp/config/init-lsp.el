904405ef0aeb35349d7370850b572a06a3193b7e
David Zhang <kiteabpl@outlook.com>
2021-02-17 21:28:35 +0800

    Use nox instead of lsp

--- site-lisp/config/init-lsp.el
+++ site-lisp/config/init-lsp.el
@@ -4,9 +4,9 @@
 ;; Description: Settings for lsp
 ;; Author: KiteAB <kiteabpl@outlook.com> (https://kiteab.ga)
 ;; Maintainer: KiteAB <kiteabpl@outlook.com> (https://kiteab.ga)
-;; Copyright (C) 2020, KiteAB, all rights reserved.
+;; Copyright (C) 2021, KiteAB, all rights reserved.
 ;; Created: 2020-12-10 20:59:12
-;; Last-Updated: 2020-12-20 18:02:37
+;; Last-Updated: 2021-02-17 21:28:32
 ;;           By: KiteAB
 ;; URL: https://github.com/KiteAB/.emacs.d/blob/master/site-lisp/config/init-lsp.el
 ;; Keywords:
@@ -61,18 +61,20 @@
                'lisp-mode-hook
                'js-mode-hook
                'web-mode-hook
-               'python-mode-hook
                'go-mode-hook
                ))
   (add-hook hook '(lambda ()
-                    (require 'lsp-mode)
-                    (lsp)
+                    ;; (require 'lsp-mode)
+                    ;; (lsp)
+                    (require 'nox)
+                    (nox-ensure)
                     )))
 
 (install-pkg 'lsp-pyright)
 (add-hook 'python-mode-hook #'(lambda ()
                                 (require 'lsp-pyright)
-                                (lsp)))
+                                ;; (lsp)
+                                (nox-ensure)))
 
 (provide 'init-lsp)
 
