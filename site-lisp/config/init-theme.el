90d2274fea4efb2cb291ee70142586950025de5a
David Zhang <kiteabpl@outlook.com>
2021-02-20 18:46:04 +0800

    Added a hook for mini-buffer in joker-theme

--- site-lisp/config/init-theme.el
+++ site-lisp/config/init-theme.el
@@ -6,7 +6,7 @@
 ;; Maintainer: KiteAB <kiteabpl@outlook.com> (https://kiteab.ga)
 ;; Copyright (C) 2021, KiteAB, all rights reserved.
 ;; Created: 2021-02-02 23:10:24
-;; Last-Updated: 2021-02-02 23:10:24
+;; Last-Updated: 2021-02-20 18:46:02
 ;;           By: KiteAB
 ;; URL: https://github.com/KiteAB/.emacs.d/blob/master/site-lisp/config/init-theme.el
 ;; Keywords:
@@ -60,6 +60,12 @@
 ;;; Code:
 
 ;; Joker Theme
+
+(add-hook 'after-make-frame-functions
+          (lambda (frame)
+            (set-window-fringes
+             (minibuffer-window frame) 0 0 nil t)))
+
 (setq joker-theme-main-color "#00AAAA")
 
 ;; Circadian
